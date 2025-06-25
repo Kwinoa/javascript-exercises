4ebc936169b55e02c036843731b97ac3301131cb
Kalina Tran <kalinathi86@gmail.com>
2025-06-24 20:03:31 -0600

    Implement sumAll function using a for loop

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if(!Number.isInteger(start) || !Number.isInteger(end) || start < 0 || end < 0){
+        return "ERROR";
+    }
+    let total = Math.min(start, end);
+    for(let i = total + 1; i <= Math.max(start, end); i++){
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
