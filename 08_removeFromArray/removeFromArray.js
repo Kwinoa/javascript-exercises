9750ae335774e96d53fd2ddb4223338f296f0b54
Kalina Tran <kalinathi86@gmail.com>
2025-06-24 19:40:30 -0600

    Implement function using filter method

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...extras) {
+    return arr.filter((item) => !extras.includes(item))
 };
 
 // Do not edit below this line
